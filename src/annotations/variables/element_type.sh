a5037699465aec535bd467115172f9ccad6b126b
Unconscionable <david.luison.starkey@gmail.com>
2022-11-13 21:34:14 +1030

    docstrings improved, function logic fixed to check typing correctly. Identified that at present, annotation will only work when a variable (which itself has a type to be checked) or command is passed to an annotated variable

--- src/annotations/variables/element_type.sh
+++ src/annotations/variables/element_type.sh
@@ -3,13 +3,49 @@ import util/types.sh
 
 
 @interface VARIABLE PRE
-# Enforces strong typing at runtime for an annotated type.
-# Can be used on variables (and commands passed as positional
-# parameters - most relevant for functions passed as arguments).
+# Enforces strong typing at runtime for an annotated type for positional parameters assigned to variables.
+# 
+# At present, only works when a type is passed as an argument (e.g. declare -i integer=22; function() "integer",
+# not function() "22")
+# 
+# Both variable and command types (e.g. "FUNCTION") can be enforced. 
+# Annotate variable declaration and/or initialisation. This annotation evaluates 
+# lazily, and will perform type checking when the annotated variable is called.
+#
+# Parameter 1: Variable or command type
+#
+# Valid values:
+# 
+# Variables: STRING, ARRAY, ASSOCIATIVE, INTEGER, NAMEREF
+#
+# Commands: ALIAS, KEYWORD, FUNCTION, BUILTIN, FILE
+#
+# Example:
+# 
+# 
+# declare -a example_array 
+# 
+# example_function() {
+# 
+# @element_type ARRAY 
+# local variable="${1}"
+# 
+# }
+# 
+# example_function "${example_array[@]}"
+#
+# Notes: 
+# 
+# ASSOCIATE=Associative array
+# 
+# If the type a name refereced (-n) variable is to be enforced, then annotate with
+# the actual variable type. 
+# 
+# If the intended type is indeed -n, specify NAMEREF. 
 element_type() {
     local type_assertion="${1}"
-    if ! check_type "${annotated_variable}" \
-    "${type_assertion}"; then
+    local value="${annotated_variable}"
+    if ! check_type "${!value}" "${type_assertion}"; then
         exit 1
     fi
 }
